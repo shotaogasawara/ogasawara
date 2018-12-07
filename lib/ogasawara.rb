1a7feba2c36d0353266f1067b939ee02d8ed2f8f
shotaogasawara <35189691+shotaogasawara@users.noreply.github.com>
2018-12-07 22:59:50 +0900

    add RDoc

--- lib/ogasawara.rb
+++ lib/ogasawara.rb
@@ -1,4 +1,13 @@
+# The main Hello driver
 class Ogasawara
+	# Say hi to gem!
+  #
+  # Example:
+  #   >> Ogasawara.hi("slovene")
+  #   => Dober dan, gem
+  #
+  # Arguments:
+  #   language: (String)
 	def self.hi(language = "english")
 		translator = Translator.new(language)
 		translator.hi
